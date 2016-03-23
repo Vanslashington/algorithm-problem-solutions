114b696e771f91faf86c15ea5451f7d7b50b9ac5
David Vaughan <DavidRayVaughan00@gmail.com>
2016-03-23 15:37:32 -0700

    Optimize #1 further using closed form sum
    Using the summation formula and inclusion-exclusion, the solution now
    runs quickly on large inputs.

--- ProjectEuler/python2/1-multiples-of-3-and-5.py
+++ ProjectEuler/python2/1-multiples-of-3-and-5.py
@@ -1,16 +1,22 @@
+def closedSum(n):
+    return (n * (n + 1)) / 2
+
 def sumOfMultiples(n):
-    return sum(x for x in xrange(3, n, 3)) + \
-           sum(x for x in xrange(5, n, 5) if x%3 != 0)
+    m = n-1
+    return 3*closedSum(m/3) + 5*closedSum(m/5) - 15*closedSum(m/15)
+    # 3 6 9 12 15 18
+    # 1 2 3 4  5  6
 
 def test():
+    assert closedSum(5) == 15
     s = sumOfMultiples
     assert s(10) == 23
     assert s(100) == 2318
     assert s(1) == 0
-    assert s(10**8) > 0
+    assert s(10**9) > 0
     print "PASSED"
-
 #test()
+
 t = int(raw_input())
 for n in (int(raw_input()) for i in xrange(t)):
     print sumOfMultiples(n)
